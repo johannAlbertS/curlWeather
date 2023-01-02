a7fdea5a43b9f6caf16a4ab954645970132a4a3e
Albert Scherzer <johann_albert.scherzer@gmx.de>
2023-01-02 19:14:31 +0100

    Changing language to english

--- weather.sh
+++ weather.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]
 then
 echo "For which city do you need a weather forecast?"
 read city
-curl -H "Accept-Language: de" "https://wttr.in/"{$city}"?ln"
+curl -H "Accept-Language: en" "https://wttr.in/"{$city}"?ln"
 exit 0
 fi
-curl -H "Accept-Language: de" "https://wttr.in/"{$1}"?ln"
+curl -H "Accept-Language: en" "https://wttr.in/"{$1}"?ln"
